802eb9c8f662865bd9b1a2b60185f60869ebaa78
Fco. Javier González Campos <a18franciscogc@iessanclemente.net>
2021-10-24 18:38:53 +0200

    DEV: simplificadas traduccións

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -25,8 +25,62 @@ const router = Router();
     res.json(req.t('WELCOME', { app: api_name, version: api_version }));
   });
 
-  // Proxectos
+  // AssignedResource
+  router.use('/assignedResources', new ProjectRoutes().getRoutes());
+
+  // AssignedStage
+  router.use('/assignedStages', new ProjectRoutes().getRoutes());
+
+  // AssignedUser
+  router.use('/assignedUsers', new ProjectRoutes().getRoutes());
+
+  // CommentApp
+  router.use('/comments', new ProjectRoutes().getRoutes());
+
+  // PerformanceApp
+  router.use('/performances', new ProjectRoutes().getRoutes());
+
+  // Priority
+  router.use('/priorities', new ProjectRoutes().getRoutes());
+
+  // Project
   router.use('/projects', new ProjectRoutes().getRoutes());
+
+  // RepositoryApp
+  router.use('/repositories', new ProjectRoutes().getRoutes());
+
+  // Requirement
+  router.use('/requirements', new ProjectRoutes().getRoutes());
+
+  // Resource
+  router.use('/resources', new ProjectRoutes().getRoutes());
+
+  // Role
+  router.use('/roles', new ProjectRoutes().getRoutes());
+
+  // Stage
+  router.use('/stages', new ProjectRoutes().getRoutes());
+
+  // State
+  router.use('/states', new ProjectRoutes().getRoutes());
+
+  // StateHistory
+  router.use('/statesHistory', new ProjectRoutes().getRoutes());
+
+  // Type
+  router.use('/types', new ProjectRoutes().getRoutes());
+
+  // User
+  router.use('/users', new ProjectRoutes().getRoutes());
+
+  // UserContact
+  router.use('/userContacts', new ProjectRoutes().getRoutes());
+
+  // UserGroup
+  router.use('/userGroups', new ProjectRoutes().getRoutes());
+
+  // UserSchedule
+  router.use('/userSchedules', new ProjectRoutes().getRoutes());
 
   return router;
 };
